0bc1c3b2f0c86830b2c75432af33ab459831a61f
silasrp_cp <silasrp@hotmail.com>
2017-01-06 10:54:06 +0000

    Initial Creation

---
+++ DUBStats/MainForm.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace DUBStats
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.selectFileLabel = new System.Windows.Forms.Label();
+            this.fileLocationTextBox = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.getStatsButton = new System.Windows.Forms.Button();
+            this.resultTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // selectFileLabel
+            // 
+            this.selectFileLabel.AutoSize = true;
+            this.selectFileLabel.Location = new System.Drawing.Point(12, 33);
+            this.selectFileLabel.Name = "selectFileLabel";
+            this.selectFileLabel.Size = new System.Drawing.Size(59, 13);
+            this.selectFileLabel.TabIndex = 0;
+            this.selectFileLabel.Text = "Select File:";
+            // 
+            // fileLocationTextBox
+            // 
+            this.fileLocationTextBox.Location = new System.Drawing.Point(78, 25);
+            this.fileLocationTextBox.Name = "fileLocationTextBox";
+            this.fileLocationTextBox.Size = new System.Drawing.Size(306, 20);
+            this.fileLocationTextBox.TabIndex = 1;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(391, 22);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.TabIndex = 2;
+            this.searchButton.Text = "Search...";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // getStatsButton
+            // 
+            this.getStatsButton.Location = new System.Drawing.Point(391, 68);
+            this.getStatsButton.Name = "getStatsButton";
+            this.getStatsButton.Size = new System.Drawing.Size(75, 23);
+            this.getStatsButton.TabIndex = 3;
+            this.getStatsButton.Text = "Get Stats!";
+            this.getStatsButton.UseVisualStyleBackColor = true;
+            this.getStatsButton.Click += new System.EventHandler(this.getStatsButton_Click);
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Location = new System.Drawing.Point(78, 68);
+            this.resultTextBox.Multiline = true;
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.ReadOnly = true;
+            this.resultTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.resultTextBox.Size = new System.Drawing.Size(306, 323);
+            this.resultTextBox.TabIndex = 4;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 403);
+            this.Controls.Add(this.resultTextBox);
+            this.Controls.Add(this.getStatsButton);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.fileLocationTextBox);
+            this.Controls.Add(this.selectFileLabel);
+            this.Name = "MainForm";
+            this.Text = "DUBStats";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label selectFileLabel;
+        private System.Windows.Forms.TextBox fileLocationTextBox;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Button getStatsButton;
+        private System.Windows.Forms.TextBox resultTextBox;
+    }
+}
+
---
+++ DUBStats/MainForm.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using Novacode;
+
+
+namespace DUBStats
+{
+    public partial class MainForm : Form
+    {
+        private readonly string _dubHeaderPattern;
+        private readonly string _dubNumberPattern;
+        private readonly string _characterHeaderPattern;
+        private readonly string _characterPattern;
+        private string _inputFile;
+        private Dictionary<string, Dictionary<int, int>> _parseResult;
+
+        public MainForm()
+        {
+            // ([0-9])+         Loop number
+            // ([_])+           Group of underlines
+            // (DUB){1}         DUB
+            // \s*              Zero or more whitespaces
+            // \[               Open square bracket
+            // ([0-9]|[:])*     A group of digits and semicolons, 0 or more times
+            // \]               Close square bracket
+            // (.)*             Qualquer character except \n, 0 or more times
+            //_dubHeaderPattern = @"^([0-9])+([_])+(DUB){1}\s+\[([0-9]{2}[:]){3}[0-9]{2}\]([\s|\t])*$";
+            _dubHeaderPattern = @"^([0-9])+([_])+(DUB){1}\s*\[([0-9]|[:])*\](.)*$";
+            _dubNumberPattern = @"([0-9])+";
+            // (\[)?            Open square bracket one or zero times
+            // ([0-9]|[:])*     A group of digits and semicolons, 0 or more times
+            // (\])?            Close square bracket one or zero times
+            // ([\s|\t])+       A group of whitespaces or tabs 1 or more times
+            // \w               A word character
+            // (.)*             Qualquer character except \n, 0 or more times
+            _characterHeaderPattern = @"^(\[)?([0-9]|[:])*(\])?([\s|\t])+\w(.)*$";
+            _characterPattern = @"([\s|\t])+(.)*";
+            InitializeComponent();
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                Title = @"Select File",
+                Filter = @"Word files|*.doc;*.docx|All Files|*.*"
+            };
+
+            if(openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                _inputFile = openFileDialog.FileName;
+                fileLocationTextBox.Text = _inputFile;
+                resultTextBox.Text = String.Empty;
+                ParseInputFile(_inputFile);
+            }
+        }
+
+        private void ParseInputFile(string filePath)
+        {
+            _parseResult = new Dictionary<string, Dictionary<int, int>>();
+            var currentLoop = 0;
+            var loopsList = new List<int>();
+            var count = 0;
+            using (var document = DocX.Load(filePath))
+            {
+                foreach (var paragraph in document.Paragraphs)
+                {
+                    if (System.Text.RegularExpressions.Regex.IsMatch(paragraph.Text, _dubHeaderPattern))
+                    {
+                        count++;
+                        var match = System.Text.RegularExpressions.Regex.Match(paragraph.Text, _dubNumberPattern);
+                        currentLoop = Convert.ToInt32(match.Value);
+                        loopsList.Add(currentLoop);
+                    }
+                    else if (System.Text.RegularExpressions.Regex.IsMatch(paragraph.Text, _characterHeaderPattern))
+                    {
+                        var match = System.Text.RegularExpressions.Regex.Match(paragraph.Text, _characterPattern);
+                        var character = match.Value.Replace("]", "");
+                        character = character.Trim();
+                        if (!_parseResult.ContainsKey(character))
+                        {
+                            _parseResult.Add(character, new Dictionary<int,int> {{currentLoop, 1}});
+                        }
+                        else
+                        {
+                            if (!_parseResult[character].ContainsKey(currentLoop))
+                            {
+                                _parseResult[character].Add(currentLoop, 1);
+                            }
+                            else
+                            {
+                                _parseResult[character][currentLoop]++;
+                            }
+                        }
+                    }
+                }
+                MessageBox.Show(@"There are " + count + @" dub headers.");
+            }
+        }
+
+        private void getStatsButton_Click(object sender, EventArgs e)
+        {
+            var text = new StringBuilder();
+            foreach (var outerKeyValuePair in _parseResult)
+            {
+                var count = 0;
+                text.AppendLine(outerKeyValuePair.Key + ": " + _parseResult[outerKeyValuePair.Key].Keys.Count + " loops.");
+                foreach (var innerKeyValuePair in _parseResult[outerKeyValuePair.Key])
+                {
+                    if (count == _parseResult[outerKeyValuePair.Key].Keys.Count)
+                    {
+                        text.AppendLine(innerKeyValuePair.Key + ".");
+                    }
+                    else
+                    {
+                        text.Append(innerKeyValuePair.Key + ", ");
+                    }
+                    count++;
+                }
+                text.AppendLine();
+                text.AppendLine();
+            }
+            resultTextBox.Text = text.ToString(0, text.Length);
+        }
+    }
+}
