e11300b1b5e99e96d017c42693d73219cbcbc3e5
silasrp_cp <SND\silasrp_cp>
2017-02-17 11:38:31 +0000

    WCF JumpStart Pizza Order App (Services and Entities)

---
+++ ZzaApp/Zza.Services/ZzaService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using Zza.Data;
+using Zza.Entities;
+
+namespace Zza.Services
+{
+    [ServiceBehavior(InstanceContextMode=InstanceContextMode.PerCall)]
+    public class ZzaService : IZzaService, IDisposable
+    {
+        private readonly ZzaDbContext _context = new ZzaDbContext();
+        public List<Product> GetProducts()
+        {
+            return _context.Products.ToList();
+        }
+
+        public List<Customer> GetCustomers()
+        {
+            return _context.Customers.ToList();
+        }
+
+        [OperationBehavior(TransactionScopeRequired=true)]
+        public void SubmitOrder(Order order)
+        {
+            _context.Orders.Add(order);
+            order.OrderItems.ForEach(oi => _context.OrderItems.Add(oi));
+            _context.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
