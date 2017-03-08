5b47a8aaf0572a3a2cd06beb3f7755f22c53c4ea
Silas Pereira <silasrp@hotmail.com>
2017-03-08 16:37:03 +0000

    test github

--- DUBStats/Program.cs
+++ DUBStats/Program.cs
@@ -14,6 +14,7 @@ namespace DUBStats
         [STAThread]
         static void Main()
         {
+            // test github
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
